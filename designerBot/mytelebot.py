d205384a397bfb356e175b90f65c82ee40fdfa0f
ALLterago <russianrarity@gmail.com>
2021-12-24 17:17:54 +1000

    обработка обойных запросов

--- designerBot/mytelebot.py
+++ designerBot/mytelebot.py
@@ -5,6 +5,20 @@ from rich.console import Console
 from telebot import types
 import botconfig
 
+category_list = {
+'1':'автомобили',
+'2':'минимализм',
+'3':'аниме',
+'4':'видеоигры',
+'5':'сериалы'}
+
+def getCategory():
+	categ_str = "Доступные категории:\n"
+	for i in range(len(category_list)):
+		categ_str += str(i+1) + ":" + str(category_list[str(i+1)]) + "\n"
+	return categ_str
+
+
 con=Console()
 banner = pyfiglet.figlet_format("designer bot ",font="banner3-D")
 bot = telebot.TeleBot(botconfig.TOKKEN)
@@ -12,6 +26,8 @@ con.print(banner)
 print("bot tokken:" + botconfig.TOKKEN)
 
 print("Пока всё работает\nПока...")
+
+
 
 #обработчик тестовых приколов
 @bot.message_handler(commands=['devtest'])
@@ -23,9 +39,24 @@ def dev_test(message):
 	except Exception:
 		print ("Ты всё сломал нахер,придурок")
 
+
+#обработчики шагов
 def sendWallpaper(message):
 		bot.send_message(message.from_user.id, str(message.from_user.id) + ":" + str(message.text))
 		print(str(message.from_user.id) + ":" + str(message.text))
+
+
+def wallHandler(message):
+	orientAllias = ['горизонтальная','вертикальная']
+	query = message.text.split(" ")
+
+	try:
+		if (len(query) != 3 or orientAllias.count(query[1]) == 0 or int(query[0]) > 5 or int(query[2]) > 10):
+			bot.send_message(message.from_user.id, "Неверный формат")
+		else:
+			bot.send_message(message.from_user.id,"Категория:" + str(category_list[str(query[0])]) + "\nОриентация:" + query[1] + "\nКоличество:" + query[2])
+	except Exception:
+		bot.send_message(message.from_user.id, "Неверный формат")
 
 #кнопка старт
 @bot.message_handler(commands=['start'])
@@ -46,6 +77,8 @@ def send_welcome(message):
 	except Exception:
 		print ("Ты всё сломал нахер,придурок")
 
+
+
 #обработчики комманд
 @bot.message_handler(commands=['help'])
 def send_help(message):
@@ -55,18 +88,21 @@ def send_help(message):
 		print ("Ты всё сломал нахер,придурок")
 
 
+
 #обратотчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
 def send_textmessage(message):
 	try:
 		if message.text == "Обои":
-			bot.send_message(message.from_user.id, "Вот твои обои:")
-			#bot.send_photo(message.from_user.id, "https://i.redd.it/2z39d50ag7w71.jpg")
-			bot.send_photo(message.from_user.id, open('profilepicture.jpg', 'rb')) 
+			bot.send_message(message.from_user.id, "Введите номер категории, ориентацию и количество")
+			bot.send_message(message.from_user.id, "Пример: 3 горизонтальная 5")
+			bot.send_message(message.from_user.id, getCategory())
+			bot.register_next_step_handler(message,wallHandler)				
+
 		elif message.text == "Рингтоны":
-			bot.send_message(message.from_user.id, "Вот твой рингтон:")
+			bot.send_message(message.from_user.id, "Функция в разработке")
 		elif message.text == "Шрифты":
-			bot.send_message(message.from_user.id, "Вот твой шрифт:")
+			bot.send_message(message.from_user.id, "Функция в разработке")
 		elif message.text == "IDDQD":
 			bot.send_message(message.from_user.id, "мальцев душнила")
 		else:
